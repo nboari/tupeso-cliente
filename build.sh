9e3b1b3008a6957c3739fbd7526c9117722e6fbd
Your Name <nicolas.boari@gmail.com>
2018-01-15 00:07:07 -0300

    build con parametros

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 polymer build
-firebase deploy
 cd build
-rm android -rf
 cordova create android com.nbormi.tupeso "Tu peso"
 cd android && rm www/* -r
 cp ../default/* www/ -r
@@ -15,4 +13,9 @@ cordova plugin add cordova-plugin-statusbar
 cordova plugin add cordova-plugin-splashscreen 
 cordova plugin add cordova-plugin-headercolor 
 cordova run android
+if [ $1 = "-f" ]; then
+    cd ../..
+    firebase login
+    firebase deploy
+fi
 exit
