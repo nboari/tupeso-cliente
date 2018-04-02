95a45d3f6cbf08f10f69454a3f6f5ece526a737f
Nicolas Boari <nicolas.boari@gmail.com>
2018-04-02 15:16:46 -0300

    cambios en la compilacion

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # -*- ENCODING: UTF-8 -*-
 polymer build
 cd build
-cordova create android com.nbormi.tupeso "Tu peso"
-cd android && rm www/* -r
+cordova create cordova com.nbormi.tupeso "Tu peso"
+cd cordova && rm www/* -r
 cp ../default/* www/ -r
 cd www/ 
 sed -i "s%<base href=\"/\">%<base href=\"file:///android_asset/www/\">%g" "index.html" 
@@ -13,7 +13,7 @@ cordova plugin add cordova-plugin-statusbar
 cordova plugin add cordova-plugin-splashscreen 
 cordova plugin add cordova-plugin-headercolor 
 cordova run android
-if [ $1 = "-f" ]; then
+if [[ $1 = "-f" ]]; then
     cd ../..
     firebase login
     firebase deploy
