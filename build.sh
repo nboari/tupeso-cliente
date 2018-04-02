27134fb3b42659d2e394330b0c475af4bad6585c
Nicolas Boari <nicolas.boari@gmail.com>
2018-04-02 18:50:36 -0300

    se mejora proceso de compilacion apk

--- build.sh
+++ build.sh
@@ -4,14 +4,15 @@ polymer build
 cd build
 cordova create cordova com.nbormi.tupeso "Tu peso"
 cd cordova && rm www/* -r
-cp ../default/* www/ -r
+cp ../polymer/* www/ -r
 cd www/ 
 sed -i "s%<base href=\"/\">%<base href=\"file:///android_asset/www/\">%g" "index.html" 
 cd .. 
-cordova platforms add android 
+cordova platforms add android
 cordova plugin add cordova-plugin-statusbar 
 cordova plugin add cordova-plugin-splashscreen 
 cordova plugin add cordova-plugin-headercolor 
+# sed -i "s%name=\"android\">%name=\"android\"><edit-config file=\"AndroidManifest.xml\" mode=\"merge\" target=\"/manifest/application\"><application android:theme=\"@android:style/Theme.Black.NoTitleBar\" /></edit-config>%g" "config.xml" 
 cordova run android
 if [[ $1 = "-f" ]]; then
     cd ../..
